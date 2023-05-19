c9de738f742d59008825c9fa819cfe4dbfa47397
Drew Sutherland <git@drewis.fun>
2023-05-19 17:40:36 -0600

    updates for latest

--- harvester/root/etc/cont-init.d/10-chiainit.sh
+++ harvester/root/etc/cont-init.d/10-chiainit.sh
@@ -2,6 +2,7 @@
 set -x
 export HOME=/chia
 
+s6-setuidgid chia chia init --fix-ssl-permissions
 s6-setuidgid chia chia init -c ${HOME}/.chia/ca
 s6-setuidgid chia chia keys add -f ${HOME}/.chia/mnemonic.txt
 
